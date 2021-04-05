f0fd34ecf158df10f4db307acc63bf8c6555c666
Nicholas Amemazior <65061618+Goldac77@users.noreply.github.com>
2021-04-05 01:27:28 +0000

    Create store manager.js

---
+++ store manager.js
@@ -0,0 +1,10 @@
+function main() {
+    var increase = parseInt(readLine(), 10);
+    var prices = [98.99, 15.2, 20, 1026];
+    //your code goes here
+    for(var i = 0; i < prices.length; i++){
+        prices[i] += increase
+    }
+    console.log(prices)
+}
+
